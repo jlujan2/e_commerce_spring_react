bfc71c6135bec9e1ad4ffbdebc203be4df3a73a2
unknown <jlujan6@gatech.edu>
2024-02-11 16:37:15 -0500

    updated code

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -7,8 +7,18 @@ import Checkout from './routes/checkout/checkout.component';
 import {useEffect} from 'react';
 import { checkUserSession } from './store/user/user.action';
 import { useDispatch } from 'react-redux';
+//import { ReactKeycloakProvider } from '@react-keycloak/web';
 
 const App = () => {
+
+  // const keycloack = new keycloack({
+  //   url: "http://localhost:8080",
+  //   realm: "spring-boot-microservices-realm",
+  //   clientId: "react-client",
+  //   onLoad: "login-required"
+  // })
+  // const initOptions = { pkceMethod: 'S256' }
+
   const dispath = useDispatch();
 
   useEffect(() => {
@@ -16,14 +26,14 @@ const App = () => {
   }, []);
 
   return (
-  <Routes>
-    <Route path='/' element={<Navigation/>}>
-      <Route index element={<Home/>}/>
-      <Route path='shop/*' element={<Shop />}/>
-      <Route path='auth' element={<Authentication />}/>
-      <Route path='checkout' element={<Checkout />}/>
-    </Route>
-  </Routes>
+    <Routes>
+      <Route path='/' element={<Navigation/>}>
+        <Route index element={<Home/>}/>
+        <Route path='shop/*' element={<Shop />}/>
+        <Route path='auth' element={<Authentication />}/>
+        <Route path='checkout' element={<Checkout />}/>
+      </Route>
+    </Routes>
   );
 };
 
